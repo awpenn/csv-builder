64c79bacec75cbd542466fd706a32a334438540d
Andrew Wilk <andrewwilk@modv-ve703-11013.apn.wlan.private.upenn.edu>
2019-11-05 15:46:49 -0500

    basic write-to-csv function working, but without column headers

--- csv_builder.js
+++ csv_builder.js
@@ -1,1 +1,28 @@
 //testing csv build for inclusion in data-portal
+const fs = require('fs')
+
+let rawdata = fs.readFileSync('./json/manifest.json')
+
+let data = JSON.parse(rawdata)
+
+function ConvertToCSV(objArray) {
+    var array = typeof objArray != 'object' ? JSON.parse(objArray) : objArray;
+    var str = '';
+
+    for (var i = 0; i < array.length; i++) {
+        var line = '';
+        for (var index in array[i]) {
+            if (line != '') line += ','
+
+            line += array[i][index];
+        }
+
+        str += line + '\r\n';
+    }
+
+    return str;
+}
+
+const csv = ConvertToCSV(data)
+
+fs.writeFileSync('./output/output.csv', csv)
